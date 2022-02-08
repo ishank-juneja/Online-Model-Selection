4d8051509816a65d944ec3a1eb1a88b1b21d9a70
Ishank Juneja <ishankjuneja@gmail.com>
2022-02-08 17:28:49 -0500

    NWI

--- src/test_cartpole_ensemble.py
+++ src/test_cartpole_ensemble.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import argparse
 import re
 from math import pi, cos, sin, atan2, sqrt
+from results_dir_manager import ResultDirManager
+dir_manager = ResultDirManager()
 
 parser = argparse.ArgumentParser()
 # Name of model being tested
@@ -14,6 +16,15 @@ parser.add_argument("--cnn-name", type=str)
 parser.add_argument("--combine", action="store_true")
 args = parser.parse_args()
 
+
+# Plots a horizontal error bar along length of entire image for perceived cart/robot position
+def plot_horizontal_error_bar(xpos, std_dev):
+    xpos_plot = sim2plot1Dx(xpos)
+    std_dev_plot = std_dev * 21
+    # print(std_dev_plot)
+    plt.axvline(xpos_plot, ymin=0, ymax=64, color='r')
+    plt.axvspan(xpos_plot - std_dev_plot, xpos_plot + std_dev_plot, ymin=0, ymax=64, alpha=0.6, color='g')
+    return
 
 # Takes the covariance matrix Sigma as an input
 def plot_ellipse(Sigma, num_sigmas=1):
@@ -40,9 +51,15 @@ def plot_ellipse(Sigma, num_sigmas=1):
         Ell_rot[:, i] = np.dot(R_rot, Ell[:, i])
     return Ell_rot
 
+def sim2plot1Dx(pos: float):
+    return int(pos * 21) + 32
+
+def sim2plot1Dy(pos: float):
+    return -int(pos * 21) + 32
+
 # Coordinate transform a point from sim to plot
 def sim2plot(point_2d_array):
-    return np.array([int(point_2d_array[0] * 21) + 32, -int(point_2d_array[1] * 21) + 32])
+    return np.array([sim2plot1Dx(point_2d_array[0]), sim2plot1Dy(point_2d_array[1])])
 
 def sim2plot_ellipse(ellipse_pts):
     npts = ellipse_pts.shape[1]
@@ -53,90 +70,76 @@ def sim2plot_ellipse(ellipse_pts):
 
 # Plot 2D mean/std-dev
 def plot_state_estimate2D(mu, std_dev):
-    # Plot ellipse center
-    mu_plot = sim2plot(mu.cpu().detach().numpy()[0])
-    std_dev = std_dev.cpu().detach().numpy()[0]
-    plt.scatter(mu_plot[0], mu_plot[1], color='g')
+    mu_plot = sim2plot(mu)
     # Arrange std_devs into covariance matrix
-    sigma = np.array([[std_dev[0] ** 2, 0.0], [0.0, std_dev[1] ** 2]])
+    std_dev = std_dev * 21
+    sigma_plot = np.array([[std_dev[0] ** 2, 0.0], [0.0, std_dev[1] ** 2]])
     # Obtain Ellipse Contour
-    ell_pts = plot_ellipse(sigma)
-    ell_pts = sim2plot_ellipse(ell_pts)
+    ell_pts = plot_ellipse(sigma_plot)
+    # print(ell_pts)
     # Plot Ellipse contour
-    plt.fill(mu_plot[0] + ell_pts[0, :] - 32, mu_plot[1] + ell_pts[1, :] - 32, alpha=0.7, color='g', ec=None)
+    plt.fill(mu_plot[0] + ell_pts[0, :], mu_plot[1] + ell_pts[1, :], alpha=0.6, color='g', ec=None)
+    # Plot ellipse center
+    plt.scatter(mu_plot[0], mu_plot[1], color='r', s=10)
 
+def plot_true_state_label(full_state):
+    obs_state = full_state[:3]
+    mass_pos = obs_state[1:]
+    cart_pos = obs_state[0]
+    plt.axvline(sim2plot1Dx(cart_pos), ymin=0, ymax=64)
+    mass_pos_plot = sim2plot(mass_pos)
+    plt.scatter(mass_pos_plot[0], mass_pos_plot[1], s=10)
 
-# Path for test data frame
+# Path to test_data_frames
 # test_path = "data/MujocoBall-v0/test_traj_22/observation_step_1.npy"
-test_path = "data-archive/Conkers-v0/all_test_observations.npy"
-# test_path =  "data/MujocoCartpole-v0/test_traj_22/observation_step_1.npy"
-state_path = "data/MujocoBall-v0/test_traj_22/traj_states.npy"
-# test_path = "data-archive/MujocoBall-v0/all_test_observations.npy"
+# test_path = "data-archive/Conkers-v0/all_test_observations.npy"
+# test_path = "data-archive/Conkers-v0/all_test_observations.npy"
+for idx in range(100):
+    for jdx in range(30):
+        test_path = "data-archive/Conkers-v0/all_test_observations.npy"
+        # test_path = "data/Kendama-v0/test_traj_{0}/observation_step_8.npy".format(idx + 1)
+        state_path = "data/MujocoBall-v0/test_traj_{0}/traj_states.npy".format(idx + 1)
+        # test_path = "data-archive/MujocoBall-v0/all_test_observations.npy"
 
-# create a model for testing
-model_name = args.cnn_name
-# model_name = 'model_conkers_Feb05_13-33-50'
-if 'conkers' in model_name:
-    from src.pendulum_analogy_config import Config
-    test_data = np.load(test_path)[57, 10, :, :, :]
-    # test_data = np.load(test_path)
-    test_states = np.load(state_path)
-elif 'ball' in model_name:
-    from src.ball_config import Config
-    test_data = np.load(test_path)
-    test_states = np.load(state_path)
-# Create pendulum analogy config object
-config = Config()
+        # create a model for testing
+        model_name = args.cnn_name
+        # model_name = 'model_conkers_Feb05_13-33-50'
+        from src.pendulum_analogy_config import Config
+        test_states = np.load(state_path)
+        if 'Conkers' in test_path:
+            test_data = np.load(test_path)[idx, jdx, :, :, :]
+        else:
+            test_data = np.load(test_path)
+        # Create pendulum analogy config object
+        config = Config()
 
-# N, T, _ = test_states.shape
-nstates = config.observation_dimension
-myensemble = UnscentedKalmanVariationalAutoencoder(config, load_name=model_name)
-if args.combine:
-    myensemble.test = True
-else:
-    myensemble.test = False
-# Use CPU at test time
-# config.device = 'cpu'
-myensemble.encoder.cuda()
+        # N, T, _ = test_states.shape
+        nstates = config.observation_dimension
+        myensemble = UnscentedKalmanVariationalAutoencoder(config, load_name=model_name)
+        if args.combine:
+            myensemble.test = True
+        else:
+            myensemble.test = False
+        # Use CPU at test time
+        # config.device = 'cpu'
+        myensemble.encoder.cuda()
 
-mu, stddev = myensemble.encode_single_observation(test_data)
+        mu, stddev = myensemble.encode_single_observation(test_data)
 
-if 'conkers' in model_name:
-    mu = mu[:, 1:]
-    stddev = stddev[:, 1:]
+        mu = mu[0].cpu().detach().numpy()
+        stddev = stddev[0].cpu().detach().numpy()
 
-print(mu)
-print(stddev)
+        print(mu)
+        print(stddev)
 
-plt.imshow(test_data)
-plot_state_estimate2D(mu, stddev)
-plt.text(10, 10, str(np.around(stddev[0].cpu().detach().numpy(), 4)), color='r', size=14)
-plt.savefig('results/tmp.png')
-
-
-
-
-# avg_stddev = 0.0
-# avg_rmse = 0.0
-#
-# # Use fewer trajectories
-# N = 5
-# for idx in range(N):
-#     print("Starting traj {0}".format(idx + 1))
-#     for jdx in range(T):
-#         test_obs = test_data[idx, jdx, :, :, :]
-#         state_label = test_states[idx, jdx, :]
-#         mu, stddev = myensemble.encode_single_observation(test_obs)
-#         # avg_rmse += np.sqrt(np.sum(np.square(mu.detach().numpy() - state_label[:3])))
-#         if not args.combine:
-#             print(mu.reshape(10, nstates))
-#             print(stddev.reshape(10, nstates))
-#         else:
-#             print(mu)
-#             print(stddev)
-#         # avg_stddev += stddev.sum()
-#         plt.imshow(test_obs)
-#         plt.show()
-# print("Average RMSE between observable part of label and prediction {0}".format(avg_rmse/(N*T)))
-# print("Average std dev for observations {0}".format(avg_stddev/(N*T)))
-
+        plt.imshow(test_data)
+        plot_state_estimate2D(mu[1:], stddev[1:])
+        plot_horizontal_error_bar(mu[0], stddev[0])
+        # plot_true_state_label(test_states[0])
+        plt.title(str(np.around(stddev, 4)), color='k', size=18)
+        dir_manager.add_location('results', 'results/')
+        fig_path = dir_manager.next_path('results', prefix='tom_cartpole_perception_', postfix='OOD_conkers%s.png')
+        plt.savefig(fig_path)
+        plt.clf()
+        plt.cla()
+        plt.close()
